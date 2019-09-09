440aab450b3e793ec84d852ed3bc42789c182964
Nicholas Jamieson <nicholas.jamieson@rexsoftware.com.au>
2019-09-10 08:12:47 +1000

    fix: Export createRuleTester.

--- source/index.ts
+++ source/index.ts
@@ -3,6 +3,7 @@
  * can be found in the LICENSE file at https://github.com/cartant/eslint-etc
  */
 
+export * from "./create-rule-tester";
 export * from "./get-loc";
 export * from "./get-parent";
 export * from "./get-parser-services";
