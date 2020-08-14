8e58be93771198a9ed3c42680ff9458a69d116ef
Nicholas Jamieson <nicholas@cartant.com>
2020-08-14 17:29:06 +1000

    feat: Add predicate overload to findParent.

--- source/find-parent.ts
+++ source/find-parent.ts
@@ -6,14 +6,36 @@
 import { TSESTree as es } from "@typescript-eslint/experimental-utils";
 import { getParent } from "./get-parent";
 
+type Predicate = (type: es.Node["type"]) => "break" | "continue" | "return";
+
 export function findParent(
   node: es.Node,
-  ...parentTypes: string[]
+  ...types: es.Node["type"][]
+): es.Node | undefined;
+
+export function findParent(
+  node: es.Node,
+  predicate: Predicate
+): es.Node | undefined;
+
+export function findParent(
+  node: es.Node,
+  ...args: (es.Node["type"] | Predicate)[]
 ): es.Node | undefined {
+  const [arg] = args;
+  const predicate: Predicate =
+    typeof arg === "function"
+      ? arg
+      : (type) => (args.indexOf(type) === -1 ? "continue" : "return");
   let parent = getParent(node);
   while (parent) {
-    if (parentTypes.indexOf(parent.type) !== -1) {
-      return parent;
+    switch (predicate(parent.type)) {
+      case "break":
+        return undefined;
+      case "return":
+        return parent;
+      default:
+        break;
     }
     parent = getParent(parent);
   }
