a6f4f569eb9f2ce625031b8566b855c39dac0474
Rafael Santana <rafaelsantana3095@gmail.com>
2021-10-13 08:09:08 -0300

    fix: accept readonly arrays in `fromFixture`'s parameters

--- source/from-fixture-spec.ts
+++ source/from-fixture-spec.ts
@@ -272,7 +272,7 @@ describe("fromFixture", () => {
             `,
           },
         ],
-      }
+      } as const
     );
     expect(test).to.have.property("code", `const name = "alice";`);
     expect(test).to.have.property("errors");
